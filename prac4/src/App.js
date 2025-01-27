2c78503517a2b7dc565c6bec43fd63ca62c7f1e6
vaishnavi9512 <vaishnavipatel.it22@scet.ac.in>
2025-01-27 09:38:44 +0000

    prac4

---
+++ prac4/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+import { CounterProvider } from './CounterContext';
+import CounterDisplay from './CounterDisplay';
+import CounterControl from './CounterControl';
+
+function App() {
+    return (
+        <CounterProvider>
+            <div className="App">
+                <CounterDisplay />
+                <CounterControl />
+            </div>
+        </CounterProvider>
+    );
+}
+
+export default App;
